a779a17219cce99203bdfea0312997f6c0adddf5
saga-mylearning <111740520+saga-mylearning@users.noreply.github.com>
2024-10-18 18:22:48 +0000

    version 1

---
+++ lesson1.py
@@ -0,0 +1,45 @@
+print(10)
+
+a=10
+print(a)
+
+
+a=10
+print("value of a=" + str(a))
+
+#operators 
+
+#Unary Operators: They work on a single operand.  
+# # Unary + and Unary —
+a=10
+
+print("value of a when Unary + =" ) 
+print(a)
+a=-10
+
+print("value of a when Unary - =" ) 
+print(a)
+#Binary Operators: They work on two operands:
+# Addition (+): To find the sum of the data values
+#Subtraction (—): To find the difference of the data values
+print(30+20) #result =50
+print(100-30) #result=70
+
+
+#Multiply (*): To find the product of the data values
+#Division (/): To divide the numbers and give the result in
+
+print(10/4) #result 2.5 #the decimal form
+
+
+#Integer Division (//): 
+# To divide the numbers and give the result in the integer form
+#Example
+print(10//4) #result =2
+
+#Modulus To divide the numbers and give the remainder
+print(10%3)
+
+#Exponential (**): To find the power of the numbers
+# 
+print(3**4)
